ace72e7d9e16eb92cd355e3722eee94f2eb4beeb
Jankosmianko <153929809+Jankosmianko@users.noreply.github.com>
2024-02-25 19:20:26 +0100

    changing searching

--- searchingAlgorythms.hpp
+++ searchingAlgorythms.hpp
@@ -1,5 +1,5 @@
 int binarySearch(int arr[], int l, int r, int x);
 int interpolationSearch(int arr[], int lo, int hi, int x);
-int searchJump(int* lista, int n, int find);
+int searchJump(int* tab, int n, int find);
 int linear(int tab[], int n, int key);
 int sentinelSearch(int arr[], int n, int key);
